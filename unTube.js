bfa2d62dbff94446ce681367b190f6c81c605ccc
Taras Shabatyn <taras@space48.com>
2021-02-24 20:29:45 +0200

    Adding launcher

--- unTube.js
+++ unTube.js
@@ -3,16 +3,31 @@
 // // recomendationsList.outerHTML = "";
 // setTimeout(function(){ console.log("hi");}, 3000);
 
-var unTube = function() {
-  var element = document.getElementById('related');
-  element.parentElement.removeChild(element);
+let launchRetries = 10;
+
+let unTube = function() {
+  let element = document.getElementById('related');
+  if (document.getElementById('playlist').getElementsByClassName('playlist-items').children === undefined) {
+    element = document.getElementById('secondary');
+  }
+  if (null !== element) {
+    launchRetries = 0;
+    element.parentElement.removeChild(element);
+  }
 };
 
-if (document.readyState == 'loading') {
+if (document.readyState === 'loading') {
   document.addEventListener('DOMContentLoaded', unTube);
 } else {
   setTimeout(unTube, 500);
 }
 
-// var element = document.getElementById('secondary');
+let launcher = function() {
+  if (launchRetries > 0) {
+    unTube();
+    setTimeout(launcher, 1000);
+  }
+}
+
+// let element = document.getElementById('secondary');
 // element.parentElement.removeChild(element);
